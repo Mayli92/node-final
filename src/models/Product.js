7900fa87c640abf8bc6087be805658197e9455d0
Mayli92 <niripil.mailen@gmail.com>
2025-11-24 10:47:27 -0300

    agregada la lógica de productos en el nuevo archivo Product.js

---
+++ src/models/Product.js
@@ -0,0 +1,9 @@
+let products = [
+    { id: 1, nombre: 'Producto 1', precio: 100 },
+    { id: 2, nombre: 'Producto 2', precio: 200 },
+    { id: 3, nombre: 'Producto 3', precio: 300 },
+];
+
+
+export const getProducts = () => {
+   return products};
